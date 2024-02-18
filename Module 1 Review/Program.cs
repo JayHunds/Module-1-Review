ae9c4ae6e3df86bde8fb3033cef1d9793f6cd173
Jayson Hundsrucker <hundsruckerjayson@gmail.com>
2024-02-18 09:05:17 -0800

    created a circle method that allows user input

---
+++ Module 1 Review/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Module_1_Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius of the circle: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = CalculateCircleArea(radius);
+
+            Console.WriteLine($"The area of the circle with radius {radius} is: {area}");
+        }
+
+        static double CalculateCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
